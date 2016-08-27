e91f489d0f11ab046af56a756c9642579e244e7a
Nathan Lo <nathanlo99@hotmail.ca>
2016-08-27 11:23:08 -0400

    Added done/vmss7wc16c5p4.py

---
+++ done/vmss7wc16c5p4.py
@@ -0,0 +1,15 @@
+n = int(input())
+x, y, z = sorted(map(int, input().split()))
+
+t = [0] + [-999999 for i in range(n)]
+for i in range(n + 1):
+    m = t[i]
+    if i >= x:
+        m = max(m, t[i - x] + 1)
+    if i >= y:
+        m = max(m, t[i - y] + 1)
+    if i >= z:
+        m = max(m, t[i - z] + 1)
+    t[i] = m
+
+print(t[n])
