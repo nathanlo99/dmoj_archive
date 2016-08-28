cdeff91dfbe12d93a44fcbc57ebe362e8a7e743d
Nathan Lo <nathanlo99@hotmail.ca>
2016-08-28 16:24:51 -0400

    Started work on graph.py

---
+++ graph.py
@@ -0,0 +1,11 @@
+#! /usr/local/bin/python3
+
+# graph.py will take an arbitrary number of command-line arguments, representing users,
+#   and plot their points over time in a pyplot graph.
+
+import matplotlib.pyplot as plt
+
+# Testing pyplot
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], "ro-")
+plt.ylabel("Some numbers")
+plt.show()
