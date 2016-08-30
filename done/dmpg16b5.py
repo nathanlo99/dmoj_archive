280aa259c51846f6d3a01d82d85b319779ab3b42
Nathan Lo <nathanlo99@hotmail.ca>
2016-08-30 15:31:57 -0400

    Added done/dmpg16b5.py

---
+++ done/dmpg16b5.py
@@ -0,0 +1,15 @@
+m, n = map(int, input().split())
+b = sorted((int(input()) for i in range(n)), reverse=True)
+
+max_v = -10000000000
+max_i = None
+sum_ = sum(b) + m
+num = n + 1
+for i, v in enumerate(b):
+    avg = sum_ / float(num)
+    if m - avg > max_v:
+        max_v = m - avg
+        max_i = i
+    sum_ -= v
+    num -= 1
+print(max_i)
