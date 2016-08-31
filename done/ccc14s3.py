54d0fba888440c879b4b05eca0e6a0a82e94bf28
Nathan Lo <nathanlo99@hotmail.ca>
2016-08-31 19:27:53 -0400

    Added done/ccc14s3.py

---
+++ done/ccc14s3.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
+    d = [int(input()) for i in range(n)]
+    next_item = 1
+    stack = []
+    while d:
+        a = d.pop()
+        if a == next_item:
+            next_item += 1
+        else:
+            stack.append(a)
+        while stack != [] and stack[-1] == next_item:
+            stack.pop()
+            next_item += 1
+
+    print("Y" if sorted(stack) == stack else "N")
