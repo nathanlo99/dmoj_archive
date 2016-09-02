a96263351233a8ddd5a37f9c814ec13a78126add
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-02 09:21:20 -0400

    Gave up mapping submission numbers to dates until API is updated

--- graph.py
+++ graph.py
@@ -8,55 +8,14 @@ import sys
 import matplotlib.pyplot as plt
 import requests
 import bs4
-import json
 
-# WARNING: SKETCHY AND INCOMPLETE
-def get_submission_dates(read_file=None, write_file=None):
-    f = None
-    date_mapping = {}
-    if read_file is not None:
-        f = open(read_file, "r")
-        data = json.loads(f.read())
-        date_mapping.update(data)
-        f.close()
-    print(date_mapping) 
-    page = 1
-    done = False
-    data_gap = 100 # Gap between submission numbers in the mapping
-    # (for example, if this is 100, we will only store submissions
-    # which are multiples of 100
-
-    while not done:
-        reply = requests.get("https://dmoj.ca/submissions/{}".format(page))
-        if reply.status_code != 200:
-            break
-        html_parser = bs4.BeautifulSoup(reply.text, "html.parser")
-        submissions_table = html_parser.find_all("table", id="submissions-table")
-        submission_nums = []
-        unix_times = []
-        submission_nums = html_parser.find_all("tr", {"id": True})
-        unix_times = html_parser.find_all("span", {"data-unix": True})
-        for submission_num, unix_time in zip(submission_nums, unix_times):
-            submission_num = submission_num["id"]
-            unix_time = unix_time["data-unix"]
-            if int(submission_num) % data_gap == 0:
-                if submission_num in date_mapping:
-                    done = True
-                    break
-                date_mapping[submission_num] = unix_time
-        print("Finished parsing page {}".format(page))
-        page += 1
-
-    if write_file is not None:
-        f = open(write_file, "w")
-        f.write(json.dumps(date_mapping))
-        f.close()
-    
-        
 
 def main():
     # TODO: Clean up code and use better variable names
     users = sys.argv[1:]
+    if users == []:
+        print("Usage: graph.py user1 [user2] ...")
+        return 0
     for user in users:
         print("Crunching data for '{}'".format(user))
         reply = requests.get("https://dmoj.ca/api/user/submissions/{}".format(user))
@@ -95,7 +54,7 @@ def main():
     plt.legend(loc=2)
     print("Plotting...")
     plt.show()
+    return 0
 
 if __name__ == "__main__":
-    main()
-    # get_submission_dates(None, "cache.txt")
+    sys.exit(main())
