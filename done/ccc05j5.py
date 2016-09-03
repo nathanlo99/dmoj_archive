0f022704cbb69c028598cbc07c5284c2a1f9e91f
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-03 12:22:38 -0400

    Added done/ccc05j5.py

---
+++ done/ccc05j5.py
@@ -0,0 +1,20 @@
+def monkey(s):
+    if a_word(s):
+        return True
+    for i, c in enumerate(s):
+        if c == "N":
+            if a_word(s[:i]) and monkey(s[i + 1:]):
+                return True
+    return False
+
+def a_word(s):
+    if s == "A":
+        return True
+    if len(s) >= 1 and s[0] == "B" and s[-1] == "S" and monkey(s[1:-1]):
+        return True
+    return False
+
+s = input()
+while s != "X":
+    print("YES" if monkey(s) else "NO")
+    s = input()
