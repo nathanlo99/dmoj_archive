e90b7ca8e783bf2548c72c61832bb9e06f427ab7
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-04 09:57:33 -0400

    Added done/rgss4.py

---
+++ done/rgss4.py
@@ -0,0 +1,11 @@
+n = int(input())
+d = [int(input()) for _ in range(n)]
+
+dp = [x for x in d]
+
+for i in range(1, n):
+    for j in range(0, i):
+        if d[i] > d[j] and dp[i] < dp[j] + d[i]:
+            dp[i] = dp[j] + d[i]
+
+print(max(dp))
