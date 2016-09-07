207f8193fb3f3197e1f78048d6e3f4d626c4f828
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-06 21:24:11 -0400

    Added done/vmss7wc15c1p3.py

---
+++ done/vmss7wc15c1p3.py
@@ -0,0 +1,35 @@
+def sieve(n):
+    sieve = [True] * n
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if sieve[i]:
+            sieve[i * i::2 * i] = [False] * ((n - i * i - 1) // (2 * i) + 1)
+    return [2] + [i for i in range(3, n, 2) if sieve[i]]
+
+primes = sieve(7010)
+def p(n):
+    if n < 2:
+        return -1
+    if n in primes:
+        return 1
+    if n % 2 == 1:
+        if n - 2 in primes:
+            return 2
+        else:
+            return 3
+    
+    for prime in primes:
+        if prime > n // 2:
+            return 3
+        if n - prime in primes:
+            return 2
+    return 3
+
+n, k = map(int, input().split())
+if k > 3:
+    print(0)
+else:
+    c = 0
+    for i in range(n + 1):
+        if p(i) == k:
+            c += 1
+    print(c)
