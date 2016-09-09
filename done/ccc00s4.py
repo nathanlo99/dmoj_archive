7d4785ecdad189c302ec897e0003b88c041b8e2c
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-09 09:23:24 -0400

    Added done/ccc00s4.py

---
+++ done/ccc00s4.py
@@ -0,0 +1,14 @@
+d = int(input())
+clubs = [int(input()) for i in range(int(input()))]
+
+dp = [100000 for i in range(d + 1)]
+dp[0] = 0
+for i in range(d + 1):
+    if dp[i] != 100000:
+        for club in clubs:
+            if i + club <= d:
+                dp[i + club] = min(dp[i + club], dp[i] + 1)
+if dp[d] == 100000:
+    print("Roberta acknowledges defeat.")
+else:
+    print("Roberta wins in {} strokes.".format(dp[d]))
