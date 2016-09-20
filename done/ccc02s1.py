36606957bc9615e032afdf78ed4de9aaeebef9da
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-20 12:35:43 -0400

    Slightly faster submissions

--- done/ccc02s1.py
+++ done/ccc02s1.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 combinations = []
 min_tickets = None
 
--- done/vmss7wc16c2p2.py
+++ done/vmss7wc16c2p2.py
@@ -4,9 +4,9 @@ input = sys.stdin.readline
 v = [0]
 a = 0
 for c in input():
-    if c == "G":
-        a += 1
+    a += (c == "G")
     v.append(a)
+
 for _ in range(int(input())):
     a, b = map(int, input().split())
     print(v[b + 1] - v[a])
