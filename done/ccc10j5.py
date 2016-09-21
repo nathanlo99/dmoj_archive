45d5af5c268c2d1b1301619ed6300778788a38ff
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-21 08:31:09 -0400

    Slightly faster submissions

--- done/ccc10j5.py
+++ done/ccc10j5.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
--- done/mwc15c8p1.py
+++ done/mwc15c8p1.py
@@ -1,5 +1,8 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 s = (n * (n + 1)) // 2
-for i in range(n - 1):
+for i in xrange(n - 1):
 	s -= int(input())
 print(s)
