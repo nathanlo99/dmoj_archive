310b47f7f1bceb8fa1b8a09d79174bed0e4469db
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-21 10:39:39 -0400

    Added done/dmopc14c3p3.py

---
+++ done/dmopc14c3p3.py
@@ -0,0 +1,18 @@
+n = int(input())
+vets = []
+for i in range(n):
+    name, skill = input().split()
+    vets.append((name, int(skill)))
+for i in range(int(input())):
+    a, d = map(int, input().split())
+    min_dist = 10000
+    min_name = ""
+    for name, skill in vets:
+        if a <= skill <= a + d:
+            if skill - a < min_dist:
+                min_name = name
+                min_dist = skill - a
+    if min_name == "":
+        print("No suitable teacher!")
+    else:
+        print(min_name)
