46bdcd6d9c23b15b6adeb3b5cbae7a9c22935022
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-22 09:44:39 -0400

    Added done/tle16c1p3.py

---
+++ done/tle16c1p3.py
@@ -0,0 +1,29 @@
+s = input()
+atoms = []
+for ch in s:
+    if ch.isalpha():
+        atoms.append((ch, 0))
+    else:
+        prev_atom, prev_amt = atoms.pop()
+        atoms.append((prev_atom, prev_amt * 10 + int(ch)))
+
+compound = {"C": 0, "H": 0, "O": 0}
+for atom, amt in atoms:
+    compound[atom] += (1 if amt == 0 else amt)
+
+c = 4 * compound["C"]
+d = 2 * compound["H"]
+b = c + compound["H"] - 2 * compound["O"]
+a = 4
+
+while a % 2 == 0 and b % 2 == 0 and c % 2 == 0 and d % 2 == 0:
+    a //= 2
+    b //= 2
+    c //= 2
+    d //= 2
+
+if min(a, b, c, d) <= 0:
+    print("Impossible")
+else:
+    a, b, c, d = map(int, (a, b, c, d))
+    print("{}{} + {}O2 -> {}CO2 + {}H2O".format(a, s, b, c, d))
