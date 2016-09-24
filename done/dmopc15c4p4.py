9e13c931c0e6b2ff4e07867684fd370addc91d09
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-24 15:54:50 -0400

    Added done/dmopc15c4p4.py

---
+++ done/dmopc15c4p4.py
@@ -0,0 +1,28 @@
+import bisect
+import sys
+input = sys.stdin.readline
+
+n, k, q = map(int, input().split())
+
+p_sum = [0]
+p_set = [[] for i in range(2001)]
+
+for i, num in enumerate(map(int, input().split())):
+    p_sum.append(p_sum[-1] + num)
+    p_set[num + 1000].append(i)
+
+def find_ge(a, x):
+    i = bisect.bisect_left(a, x)
+    if i == len(a):
+        return 100000000
+    return a[i]
+
+for _ in range(q):
+    a, b, x, y = map(int, input().split())
+    if p_sum[y] - p_sum[x - 1] > k:
+        pa = p_set[a + 1000]
+        pb = p_set[b + 1000]
+        if find_ge(pa, x - 1) <= y - 1 and find_ge(pb, x - 1) <= y - 1:
+            print("Yes")
+            continue
+    print("No")
