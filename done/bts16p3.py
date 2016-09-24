2c4f6d2021c3ec96d55239a7f1b893d3a01d5300
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-24 15:54:55 -0400

    Slightly faster submissions

--- done/bts16p3.py
+++ done/bts16p3.py
@@ -1,13 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 last = ""
+last_value = 1
 ans = 0
-dp = [0 for i in range(n)]
 for i, name in enumerate(input().split()):
     if name[0] == last:
-        ans += dp[i - 1] + 1
-        dp[i] = dp[i - 1] + 1
+        last_value += 1
+        ans += last_value
     else:
         ans += 1
-        dp[i] = 1
+        last_value = 1
     last = name[0]
 print(ans)
--- done/coci14c6p2.py
+++ done/coci14c6p2.py
@@ -1,10 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 formations = []
-for i in range(int(input())):
+for i in xrange(int(input())):
     o, v, n = map(int, input().split('-'))
     formations.append((o, v, n))
 
 poss = set({(0, 0, 0)})
-for _ in range(int(input())):
+for _ in xrange(int(input())):
     new = set()
     s = input()
     if "O" in s:
@@ -17,5 +20,6 @@ for _ in range(int(input())):
         for o, v, n in poss:
             new.add((o, v, n + 1))
     poss.update(new)
+
 for formation in formations:
     print("DA" if formation in poss else "NE")
