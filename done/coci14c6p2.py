6c7751f205c79293082e81a126d2defedf4ed013
Nathan Lo <nathanlo99@hotmail.ca>
2016-09-24 13:14:58 -0400

    Added done/coci14c6p2.py

---
+++ done/coci14c6p2.py
@@ -0,0 +1,21 @@
+formations = []
+for i in range(int(input())):
+    o, v, n = map(int, input().split('-'))
+    formations.append((o, v, n))
+
+poss = set({(0, 0, 0)})
+for _ in range(int(input())):
+    new = set()
+    s = input()
+    if "O" in s:
+        for o, v, n in poss:
+            new.add((o + 1, v, n))
+    if "V" in s:
+        for o, v, n in poss:
+            new.add((o, v + 1, n))
+    if "N" in s:
+        for o, v, n in poss:
+            new.add((o, v, n + 1))
+    poss.update(new)
+for formation in formations:
+    print("DA" if formation in poss else "NE")
