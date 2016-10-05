1627d4091306b15fd5280d72b8737cf8d4ae9cf3
Nathan Lo <nathanlo99@hotmail.ca>
2016-10-05 16:58:03 -0400

    Modified done/mwc15c2p2.py

--- done/mwc15c2p2.py
+++ done/mwc15c2p2.py
@@ -3,11 +3,10 @@ input = sys.stdin.readline
 
 input()
 stack = [(1000000, 0)]
-
-ans = []
+ans = ""
 for index, tower in enumerate(map(int, input().split())):
     while tower >= stack[-1][0]:
         stack.pop()
-    ans.append(index - stack[-1][1])
+    ans += str(index - stack[-1][1]) + " "
     stack.append((tower, index))
-print(" ".join(map(str, ans)))
+print(ans)
