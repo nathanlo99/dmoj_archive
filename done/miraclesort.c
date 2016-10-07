1da9b381b388e0ee41eee06b4881f81ee6b937cb
Nathan Lo <nathanlo99@hotmail.ca>
2016-10-07 16:05:13 -0400

    Re-downloaded all files from DMOJ

--- done/miraclesort.c
+++ done/miraclesort.c
@@ -1,4 +1,1 @@
-main(int _, char** __) {
-    while (getchar_unlocked()!='\n');
-    while ((**__=getchar_unlocked())!=-1) putchar_unlocked(**__);
-}
+main(int _,char** __){while(getchar_unlocked()!='\n');while((**__=getchar_unlocked())!=-1)putchar_unlocked(**__);}
