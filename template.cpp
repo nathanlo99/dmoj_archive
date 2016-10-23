0bd58f4ea1e94f5ddcff78cde005db5423d10d90
Nathan Lo <nathanlo99@hotmail.ca>
2016-10-22 23:52:45 -0400

    Added fast input template

---
+++ template.cpp
@@ -0,0 +1,5 @@
+#define getchar() (*_pinp?*_pinp++:(_inp[fread(_pinp=_inp, 1, 4096, stdin)]='\0', *_pinp++))
+#define scan(x) do{while((x=getchar())<'-'); _ssign=x=='-'; if(_ssign) while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0'); x=_ssign?-x:x;}while(0)
+
+char _inp[4097], *_pinp=_inp, _;
+int _ssign;
