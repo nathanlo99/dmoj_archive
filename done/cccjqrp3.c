7e1b61d9c21d05d03d2ce8939ca8305168e355f9
Nathan Lo <nathanlo99@hotmail.ca>
2016-10-28 17:54:42 -0400

    Re-downloaded all files from DMOJ

--- done/cccjqrp3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-unsigned long long a, b, c;
-
-int main() {
-    scanf("%llu %llu %llu", &a, &b, &c);
-    printf("%llu\n", (a + b + c) % 42069900169420);
-}
---
+++ done/cccjqrp3.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+unsigned long long a, b, c;
+
+int main() {
+    scanf("%llu%llu%llu", &a, &b, &c);
+    printf("%llu\n", (a + b + c) % 42069900169420ULL);
+}
