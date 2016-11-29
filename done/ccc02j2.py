65afec007b1ade271a0d55adb4685150f207160a
Nathan Lo <nathanlo99@hotmail.ca>
2016-11-29 09:41:47 -0500

    Slightly faster submissions

--- done/ccc02j2.py
+++ done/ccc02j2.py
@@ -1,6 +1,9 @@
-d = input()
-while(d != 'quit!'):
+import sys
+input = sys.stdin.readline
+
+d = input().strip()
+while (d != "quit!"):
 	if len(d) > 4 and d[-3] not in ['a', 'e', 'i', 'o', 'u'] and d[-2:] == "or":
 		d = d[:-1] + "u" + d[-1]
 	print(d)
-	d = input()
+	d = input().strip()
