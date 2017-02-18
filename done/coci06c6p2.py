2c6d10218444e19a2f181c03a8d242d4a93eb523
Nathan Lo <nathanlo99@hotmail.ca>
2017-02-18 08:27:46 -0500

    Added done/coci06c6p2.py

---
+++ done/coci06c6p2.py
@@ -0,0 +1,18 @@
+
+r, c = map(int, input().split())
+s = [input() for _ in range(r)]
+a, b = map(int, input().split())
+
+for i in range(2 * r):
+    line = ""
+    for j in range(2 * c):
+        if i >= r: x = 2 * r - i - 1
+        else: x = i
+        if j >= c: y = 2 * c - j - 1
+        else: y = j
+        char = s[x][y]
+        if (i, j) == (a - 1, b - 1):
+            char = {"#": ".", ".": "#"} [char]
+        line += char
+    print(line)
+
