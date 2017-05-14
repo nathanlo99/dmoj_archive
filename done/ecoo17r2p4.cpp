a95d04f9b7dec5db8846ad34305d5b6470818e19
Nathan Lo <nathanlo99@hotmail.ca>
2017-05-14 07:42:35 -0400

    Added done/ecoo17r2p4.cpp

---
+++ done/ecoo17r2p4.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+
+int n;
+long long last[10005], cur[10005];
+
+void do_case(){
+    scanf("%d",&n);
+    n--;
+    last[0]=cur[0]=1;
+    for (int i=1; i<=n; i++) {
+        for (int j=0; j<=i; j++) cur[i-j]=last[j];
+        for (int j=1; j<=i; j++) cur[j]=(cur[j]+cur[j-1])%MOD;
+        for (int i = 0; i < 10005; i++) last[i] = cur[i];
+    }
+    int ans=0;
+    for (int i=0; i<=n; i++) ans=(ans+cur[i])%MOD;
+    printf("%d\n",ans);
+}
+
+int T = 10;
+
+int main(){
+    while (T--) do_case();
+}
