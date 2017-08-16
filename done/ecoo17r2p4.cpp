6c0c1b60ab27eb6e1ed008ba4b7c40c93d479e83
Nathan Lo <nathanlo99@hotmail.ca>
2017-08-15 21:42:40 -0400

    Fixed formatting and made it run much faster

--- done/ecoo17r2p4.cpp
+++ done/ecoo17r2p4.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
-#define MOD 1000000007
+#define mod 1000000007
 
-int n;
-long long last[10005], cur[10005];
+int n, last[10005], cur[10005];
 
 void do_case(){
-    scanf("%d",&n);
+    scanf("%d",&n); n--;
     memset(last, 0, sizeof(last));
     memset(cur, 0, sizeof(cur));
-    n--;
-    last[0]=cur[0]=1;
-    for (int i=1; i<=n; i++) {
-        for (int j=0; j<=i; j++) cur[i-j]=last[j];
-        for (int j=1; j<=i; j++) cur[j]=(cur[j]+cur[j-1])%MOD;
+    
+    last[0] = cur[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= i; j++) cur[i - j] = last[j];
+        for (int j = 1; j <= i; j++) cur[j] = (cur[j] + cur[j - 1]) % mod;
         for (int i = 0; i < 10005; i++) last[i] = cur[i];
     }
-    int ans=0;
-    for (int i=0; i<=n; i++) ans=(ans+cur[i])%MOD;
-    printf("%d\n",ans);
+    
+    int ans = 0;
+    for (int i = 0; i <= n; i++) ans = (ans + cur[i]) % mod;
+    printf("%d\n", ans); 
 }
 
-int T = 10;
+int t = 10;
 
-int main(){
-    while (T--) do_case();
+int main() {
+    while (t --> 0) do_case();
 }
