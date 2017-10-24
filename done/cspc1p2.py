e0460c141f59cc62898b2faad9f2ae6fb935977d
Nathan Lo <nathanlo99@hotmail.ca>
2017-10-24 14:05:08 -0400

    Some problems got bamboozled

--- done/cspc1p2.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-w = int(input())
-h = int(input()) if w > 0 else 0
-
-print("It takes {} hours for Team 610 build a robot.".format(n * (w - 1) + (n + h) * (6 - w + 1)))
--- done/vmss7wc15c6p1.py
+++
@@ -1,6 +0,0 @@
-count = [0 for _ in range(26)]
-for c in input():
-    if c != ' ': count[(ord(c) - min(ord('A'), ord('a'))) % 32] += 1
-for c in input():
-    if c != ' ': count[(ord(c) - min(ord('A'), ord('a'))) % 32] -= 1
-print("yes" if all(x == 0 for x in count) else "no")
