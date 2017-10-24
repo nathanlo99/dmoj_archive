53218b98ecc6dee20e8f53abf0f5e755382e7b1b
Nathan Lo <nathanlo99@hotmail.ca>
2017-10-24 13:54:49 -0400

    Added done/bbc08d.cpp

---
+++ done/bbc08d.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    printf("1998\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 2; j <= 1000; j++) {
+            printf("%d 1 %d 2\n", j, j);
+        }
+    }
+    return 0;
+}
