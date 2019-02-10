61187b64e8f11059aa7fba6933b1e75af44b0963
Nathan Lo <nathanlo99@hotmail.ca>
2019-02-09 21:38:08 -0500

    Removed cco07p2.py

--- done/cco07p2.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-a = set()
-for i in range(n):
-    s = tuple(sorted(map(int, input().split())))
-    a.add(s)
-if len(a) == n:
-    print("No two snowflakes are alike.")
-else:
-    print("Twin snowflakes found.")
